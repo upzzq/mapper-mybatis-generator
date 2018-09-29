0aa59615a35849858039433bdb8651ed80f3ade7
upzzq <34890233+upzzq@users.noreply.github.com>
2018-09-29 14:22:40 +0800

    初始导入

---
+++ src/main/java/com/generator/MapperGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperGeneratorApplication.class, args);
+	}
+}
